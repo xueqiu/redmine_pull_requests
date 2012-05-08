3e5f6a3042924dbc5d6f4ffd080963699ae78a2c
pawa <pawamaba@gmail.com>
2012-05-09 01:05:48 +0800

    change version to 0.1

--- init.rb
+++ init.rb
@@ -26,7 +26,7 @@ Redmine::Plugin.register :redmine_pull_requests do
   name 'Redmine Pull Requests plugin'
   author 'pawa'
   description 'github like pull requests for redmine'
-  version '0.0.1'
+  version '0.1'
   url 'http://github.com/pawa/redmine_pull_requests'
   author_url 'http://pawa.github.com'
 

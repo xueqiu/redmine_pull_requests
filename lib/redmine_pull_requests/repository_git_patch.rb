28fb6df85f9f9f75e3d9273e908dacd4553d83c2
pawa <pawamaba@gmail.com>
2012-05-15 17:08:57 +0800

    use redmine diff method with three dot append to rivision

--- lib/redmine_pull_requests/repository_git_patch.rb
+++ lib/redmine_pull_requests/repository_git_patch.rb
@@ -1,8 +1,11 @@
+require 'redmine/scm/adapters/abstract_adapter'
+require 'redmine/scm/adapters/git_adapter'
+
 module RedminePullRequests
   module RepositoryGitPatch
     
     def self.included(base)
-      #base.extend(ClassMethods)
+#      base.send(:extend, ClassMethods)
       base.send(:include, InstanceMethods)
     end
     
@@ -22,7 +25,6 @@ module RedminePullRequests
           ],
           :order => 'committed_on DESC'
         )
-        
       end
 
       def branch_ancestor(base_branch, head_branch)
@@ -34,27 +36,6 @@ module RedminePullRequests
         end
         revision ? revision.gsub("\n", "").strip : '+-'
       end
-      
-      def diff_with_three_dot(path, identifier_from, identifier_to=nil)
-        path ||= ''
-        cmd_args = []
-        if identifier_to
-          cmd_args << "diff" << "--no-color" <<  identifier_to << "..." << identifier_from
-        else
-          cmd_args << "show" << "--no-color" << identifier_from
-        end
-        cmd_args << "--" <<  scm_iconv(@path_encoding, 'UTF-8', path) unless path.empty?
-        diff = []
-        git_cmd(cmd_args) do |io|
-          io.each_line do |line|
-            diff << line
-          end
-        end
-        diff
-      rescue ScmCommandAborted
-        nil
-      end      
     end
-  
   end
 end

ee086b871cee1c52c649a38c8332e4e73b442baa
pawa <pawamaba@gmail.com>
2012-05-25 19:11:42 +0800

    add close pull email notification

--- app/models/pull_mailer.rb
+++ app/models/pull_mailer.rb
@@ -11,5 +11,20 @@ class PullMailer < Mailer
          :pull_url => url_for(:controller => 'pulls', :action => 'show', :project_id => pull.project.identifier, :id => pull.id)
     render_multipart('settings/pull_add', body)
   end
+  
+  def pull_close(pull)
+    if pull.status == 'closed'
+      redmine_headers 'Project' => pull.project.identifier,
+                      'Pull-Request-Id' => pull.id,
+                      'Pull-Request-Author' => pull.user
+      message_id pull
+      recipients [Setting.plugin_redmine_pull_requests['default_sent_to_email']]
+      subject "[#{pull.project.name} - Pull request ##{pull.id}] (#{pull.status.camelize}) #{pull.title}"
+      body :pull => pull,
+           :pull_url => url_for(:controller => 'pulls', :action => 'show', :project_id => pull.project.identifier, :id => pull.id)
+      render_multipart('settings/pull_close', body)
+    end
+  end
+  
 
 end

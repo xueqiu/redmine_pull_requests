807d4b31c249ce5a15362f9a46ac6b997f47b3ea
pawa <pawamaba@gmail.com>
2012-05-29 19:12:33 +0800

    change close pull observer to pull_item_observer

--- app/models/pull_observer.rb
+++ app/models/pull_observer.rb
@@ -1,11 +1,5 @@
 class PullObserver < ActiveRecord::Observer
   def after_create(pull)
     PullMailer.deliver_pull_add(pull) #if Setting.notified_events.include?('issue_added')
-  end
-  
-  def after_update(pull)
-    if pull.status == 'closed'
-      PullMailer.deliver_pull_close(pull) #if Setting.notified_events.include?('issue_added')
-    end
-  end  
+  end 
 end

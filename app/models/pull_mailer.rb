565b4877ca3fb081408b1f1131cc1ef46dc370b6
pawa <pawamaba@gmail.com>
2012-05-30 11:52:03 +0800

    change recipients to repo comnitters plus default mail

--- app/models/pull_mailer.rb
+++ app/models/pull_mailer.rb
@@ -7,7 +7,7 @@ class PullMailer < Mailer
                     'Pull-Request-Id' => pull.id,
                     'Pull-Request-Author' => pull.user
     message_id pull
-    recipients [Setting.plugin_redmine_pull_requests['default_sent_to_email']]
+    recipients find_recipients(pull.repository)
     subject "[#{pull.project.name} - Pull request ##{pull.id}] #{pull.title}"
     body :pull => pull,
          :pull_url => url_for(:controller => 'pulls', :action => 'show', :project_id => pull.project.identifier, :id => pull.id)
@@ -22,7 +22,7 @@ class PullMailer < Mailer
                     'Pull-Request-Id' => pull.id,
                     'Pull-Request-Author' => pull.user
     message_id pull
-    recipients [Setting.plugin_redmine_pull_requests['default_sent_to_email']]
+    recipients find_recipients(pull.repository)
     subject "[#{pull.project.name} - Pull request ##{pull.id}] (#{item.item_type.camelize}) #{pull.title}"
     body :pull => pull, :item => item,
          :pull_url => url_for(:controller => 'pulls', :action => 'show', :project_id => pull.project.identifier, :id => pull.id)
@@ -37,11 +37,19 @@ class PullMailer < Mailer
                     'Pull-Request-Id' => pull.id,
                     'Pull-Request-Author' => pull.user
     message_id pull
-    recipients [Setting.plugin_redmine_pull_requests['default_sent_to_email']]
+    recipients  find_recipients(pull.repository)
     subject "[#{pull.project.name} - Pull request ##{pull.id}] (#{item.item_type.camelize}) #{pull.title}"
     body :pull => pull, :item => item,
          :pull_url => url_for(:controller => 'pulls', :action => 'show', :project_id => pull.project.identifier, :id => pull.id)
     render_multipart('settings/pull_comment', body)
   end  
+
+  private
+  def find_recipients(repo)
+    default = Setting.plugin_redmine_pull_requests['default_sent_to_email']
+    recipients = User.find_all_by_id(repo.committers.collect(&:last).collect(&:to_i)).collect(&:mail)
+    recipients += [default] if !recipients.include?(default)
+    recipients
+  end
 
 end

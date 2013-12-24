cc639d87b555dc9cefd659d1b6cd247e81696124
Croath <croathliu@gmail.com>
2013-12-24 17:57:55 +0800

    lambda udpates

--- app/models/pull_item.rb
+++ app/models/pull_item.rb
@@ -4,6 +4,6 @@ class PullItem < ActiveRecord::Base
   belongs_to :pull
   belongs_to :user
   
-  scope :with_status, -> (pull_id, status) { :conditions => { :pull_id => pull_id, :item_type => status } }
+  scope :with_status, -> (pull_id, status) { where(pull_id: pull_id, item_type: status) }
   
 end

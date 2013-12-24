2da154b41bbd02e1f2af7fd8e389642306e1c301
Croath <croathliu@gmail.com>
2013-12-24 13:38:31 +0800

    make it clear

--- app/models/pull_item.rb
+++ app/models/pull_item.rb
@@ -4,8 +4,6 @@ class PullItem < ActiveRecord::Base
   belongs_to :pull
   belongs_to :user
   
-  scope :with_status, lambda { |pull_id, status|
-    { :conditions => { :pull_id => pull_id, :item_type => status } }
-  }  
+  scope :with_status, -> (pull_id, status) { :conditions => { :pull_id => pull_id, :item_type => status } }
   
 end
